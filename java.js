0755a24e5ce97e491723859e2dbe3380d84c7e9b
Tayo46 <63529655+Tayo46@users.noreply.github.com>
2020-04-21 01:02:09 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,14 @@
+// navbar
+
+var navList = document.getElementById("nav-lists");
+function Show() {
+navList.classList.add("_Menus-show");
+}
+
+function Hide(){
+navList.classList.remove("_Menus-show");
+}
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
